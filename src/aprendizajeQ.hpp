18b8aacf2d1106daa484be27fde713f56779d481
julianaleon8 <julianaleon8@gmail.com>
2014-10-26 21:10:12 -0430

    con casi todos los comentarios y casi compila :)

--- src/aprendizajeQ.hpp
+++ src/aprendizajeQ.hpp
@@ -3,33 +3,46 @@
 #include <fstream>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+#ifndef APRENDIZAJEQ
 #define APRENDIZAJEQ
 
 /*
  * Aprendizaje por Reforzamiento
  * Las recompensas se definen de [0-1]
  * Los Castigos se defien de [0-1]
+ * Las Acciones se definen de la siguiente manera:
+ * a0 = caminar poco
+ * a1 = caminar
+ * a2 = caminar mucho
+ * a3 = girar derecha
+ * a4 = girar izquierda
+ * a5 = girar poco derecha
+ * a6 = girar poco izquierda
  */
+
 #define K 1
 #define NUM_ESTADOS 14
 #define NUM_ACCION 7
 namespace AprendizajeQ {
-  int estadoViejo ;
-  int accion = -1;
-  int Q[NUM_ESTADOS][NUM_ACCION];
 
-  void leerValores();
-  //void escribirValores();
-
-  void tomarAccion(int);
-  
-  void actualizarValor(int);
-
-  int recompensa(int,int);
-  int distancia(int);
-  int maxQ(int);
-
-
+    // estado anterior	
+	int estadoViejo ;
+	// accion anterior es -1 si es la primera vez
+	int accion = -1;
+	
+	int Q[NUM_ESTADOS][NUM_ACCION];
+	
+	void leerValores();
+	void escribirValores();
+	
+	void tomarAccion(int);
+	
+	void actualizarValor(int);
+	
+	int recompensa(int,int);
+	int distancia(int);
+	int maxQ(int);
 }
 
 #endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "aprendizajeQ.hpp"
+
+int main(){
+
+	return 0;
+
+}
